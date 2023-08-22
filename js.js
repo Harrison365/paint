b8997178c47147f1d6321c1d8de0a5cb7e797139
Harrison Royds <harrisonroyds@yahoo.co.uk>
2023-08-22 17:09:43 +0100

    altered canvas margins'

--- js.js
+++ js.js
@@ -2,8 +2,8 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 
-canvas.width = 1200;
-canvas.height = 800;
+canvas.width = 900;
+canvas.height = 600;
 
 let isDrawing = false;
 let x, y;
